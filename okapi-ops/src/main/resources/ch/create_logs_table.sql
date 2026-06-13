de25b5da89746c6bea3823587922d9a53782a4ab
Kushal Sharma <sharmakushal2011@gmail.com>
2026-06-13 01:25:17 +0100

    Add ClickHouse logs table

---
+++ okapi-ops/src/main/resources/ch/create_logs_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS okapi_logs.logs_table_v1
+(
+    ts_ns Int64,
+    log_stream LowCardinality(String),
+    service_name LowCardinality(String),
+    log_level Int32,
+    body String CODEC(ZSTD),
+    INDEX body_text_idx(body) TYPE text() GRANULARITY 1
+)
+ENGINE = MergeTree
+PARTITION BY toStartOfHour(toDateTime(ts_ns / 1000000000))
+ORDER BY (ts_ns, log_stream, service_name, log_level);
